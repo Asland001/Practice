9196222337960ada620c43f6b46b53f6957beac3
Asland001 <artyem.senkevich@gmail.com>
2026-03-17 11:26:07 +0300

    Add files via upload

---
+++ Practice A/Zadanie 1/Zadanie 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите величину временного интервала (в минутах):");
+
+            int totalMinutes = int.Parse(Console.ReadLine());
+
+            int hours = totalMinutes / 60;
+            int minutes = totalMinutes % 60;
+
+            Console.WriteLine("{0} минут — это {1}ч. {2} мин.", totalMinutes, hours, minutes);
+        }
+    }
+}
