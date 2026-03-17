7b97ddfa72531d917c81b613c53706aeccadfa7e
Asland001 <artyem.senkevich@gmail.com>
2026-03-17 11:28:56 +0300

    Add files via upload

---
+++ Practice A/Zadanie 2/Zadanie 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumberPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число, в котором все цифры различны:");
+
+            int number = int.Parse(Console.ReadLine());
+
+            int digit1 = number / 100;
+            int digit2 = (number / 10) % 10;
+            int digit3 = number % 10;
+
+            int num1 = digit1 * 100 + digit2 * 10 + digit3;
+            int num2 = digit1 * 100 + digit3 * 10 + digit2;
+            int num3 = digit2 * 100 + digit1 * 10 + digit3;
+            int num4 = digit2 * 100 + digit3 * 10 + digit1;
+            int num5 = digit3 * 100 + digit1 * 10 + digit2;
+            int num6 = digit3 * 100 + digit2 * 10 + digit1;
+
+            Console.WriteLine("Числа, полученные перестановкой цифр:");
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+            Console.WriteLine(num4);
+            Console.WriteLine(num5);
+            Console.WriteLine(num6);
+        }
+    }
+}
---
+++ Practice A/Zadanie 3/Zadanie 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FormulaCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            double z1 = ((a + 2) / Math.Sqrt(2 * a)
+                         - a / Math.Sqrt(2 * a + 2)
+                         + 2 / (a - Math.Sqrt(2 * a)))
+                * (Math.Sqrt(a) - Math.Sqrt(2)) / (a + 2);
+
+            double z2 = 1 / (Math.Sqrt(a) + Math.Sqrt(2));
+
+            Console.WriteLine($"z1 = {z1}");
+            Console.WriteLine($"z2 = {z2}");
+        }
+    }
+}
