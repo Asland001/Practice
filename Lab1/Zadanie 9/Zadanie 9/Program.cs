c820e14c9b9a90004ef3710cc706e8df913d898e
Asland001 <artyem.senkevich@gmail.com>
2026-03-18 15:02:52 +0300

    Add files via upload

---
+++ Lab1/Zadanie 9/Zadanie 9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double A = 0.0;
+        double B = Math.PI / 2;
+        int M = 20;
+
+        double H = (B - A) / M;
+
+        Console.WriteLine("Табулирование функции F(x) = sin(x) - cos(x)");
+        Console.WriteLine($"Диапазон: [{A:F4}, {B:F4}], шаг H = {H:F6}, точек: {M+1}\n");
+        Console.WriteLine("   №   |      x       |         F(x)");
+        Console.WriteLine("-------|--------------|---------------------");
+
+        double x = A;
+
+        for (int i = 1; i <= M + 1; i++)
+        {
+            double y = Math.Sin(x) - Math.Cos(x);
+
+            Console.WriteLine($"{i,4}   | {x,12:F6} | {y,19:F10}");
+
+            x = x + H;
+        }
+    }
+}
---
+++ Lab1/Zadanie dopolnitelno/Zadanie dopolnitelno/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Двузначные числа, равные утроенному произведению своих цифр:\n");
+
+        int count = 0;
+
+        for (int n = 10; n <= 99; n++)
+        {
+            int a = n / 10;
+            int b = n % 10;
+
+            if (n == 3 * a * b)
+            {
+                Console.WriteLine($"{n} = 3 * {a} * {b} = 3 * {a * b}");
+                count++;
+            }
+        }
+
+        if (count == 0)
+        {
+            Console.WriteLine("Таких чисел не найдено.");
+        }
+        else
+        {
+            Console.WriteLine($"\nВсего найдено: {count} чисел");
+        }
+    }
+}
