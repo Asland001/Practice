01c93cfbf0934bf3cf1e8421109977887382b370
Asland001 <artyem.senkevich@gmail.com>
2026-03-19 13:57:09 +0300

    Add files via upload

---
+++ Lab 3/Zadanie 1/Zadanie 1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassZadanie
+{
+    class A
+    {
+        private int a;
+        private int b;
+        
+        public A(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        //sin(b) + 4/(2a)
+        public double CalculateExpression()
+        {
+            return Math.Sin(b) + 4.0 / (2 * a);
+        }
+
+        //(a + b)^2
+        public int SquareSum()
+        {
+            return (a + b) * (a + b);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            A obj = new A(a, b);
+
+            Console.WriteLine($"sin(b) + 4/(2a) = {obj.CalculateExpression():F4}");
+            Console.WriteLine($"(a + b)^2 = {obj.SquareSum()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 3/Zadanie 2/Zadanie 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace StaticArrayZadanie
+{
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    
+    static class ArrayUtils
+    {
+        public static void ShuffleArray(Person[] people)
+        {
+            Random rnd = new Random();
+
+            for (int i = people.Length - 1; i > 0; i--)
+            {
+                int j = rnd.Next(i + 1);
+                
+                Person temp = people[i];
+                people[i] = people[j];
+                people[j] = temp;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person[] people =
+            {
+                new Person("Артём", 18),
+                new Person("Иван", 20),
+                new Person("Оля", 19),
+                new Person("Макс", 21)
+            };
+
+            Console.WriteLine("До перемешивания:");
+            PrintArray(people);
+
+            ArrayUtils.ShuffleArray(people);
+
+            Console.WriteLine("\nПосле перемешивания:");
+            PrintArray(people);
+
+            Console.ReadKey();
+        }
+        
+        static void PrintArray(Person[] people)
+        {
+            foreach (var p in people)
+            {
+                Console.WriteLine($"{p.Name}, {p.Age}");
+            }
+        }
+    }
+}
