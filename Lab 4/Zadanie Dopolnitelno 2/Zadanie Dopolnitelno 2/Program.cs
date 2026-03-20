8af47d97bd0cb5717a9a080acf0a2716824a9ddb
Asland001 <artyem.senkevich@gmail.com>
2026-03-20 22:07:05 +0300

    Add files via upload

---
+++ Lab 4/Zadanie Dopolnitelno 2/Zadanie Dopolnitelno 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LogZadanie 
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(LogMessage("Сервер запущен"));
+            Console.WriteLine(LogMessage("Произошла ошибка", "404"));
+
+            Console.ReadKey();
+        }
+
+        //Обычное сообщение
+        static string LogMessage(string message)
+        {
+            return message;
+        }
+
+        //Сообщение с ошибкой
+        static string LogMessage(string message, string errorCode)
+        {
+            return $"{message}, Code: {errorCode}";
+        }
+    }
+}
---
+++ Lab 4/Zadanie Dopolnitelno 3/Zadanie Dopolnitelno 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CurrencyTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            double result;
+            double amount = 690;
+            
+            ConvertCurrency(in amount, out result);
+            Console.WriteLine($"Результат (фиксированный курс): {result}");
+            
+            ConvertCurrency(in amount, 1.2, out result);
+            Console.WriteLine($"Результат (курс 1.2): {result}");
+
+            Console.ReadKey();
+        }
+
+        //Метод с фиксированным курсом
+        static void ConvertCurrency(in double amount, out double convertedAmount)
+        {
+            double fixedRate = 1.1;
+            convertedAmount = amount * fixedRate;
+        }
+
+        //Перегруженный метод
+        static void ConvertCurrency(in double amount, double exchangeRate, out double convertedAmount)
+        {
+            convertedAmount = amount * exchangeRate;
+        }
+    }
+}   
