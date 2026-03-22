65180f57b0186b49127628c631b0d2d0ae4c8a43
Asland001 <artyem.senkevich@gmail.com>
2026-03-23 00:28:28 +0300

    Add files via upload

---
+++ Lab 5/Zadanie 3/Zadanie 3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace SportsAppZadanie
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+
+        public TeamMember(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Игрок: {Name}");
+        }
+    }
+
+    //Интерфейс для баскетболистов
+    public interface IBasketballPlayer
+    {
+        void Shoot();
+    }
+
+    //Интерфейс для футболистов
+    public interface IFootballPlayer
+    {
+        void Kick();
+    }
+
+    //Класс баскетболиста
+    public class Basketballer : TeamMember, IBasketballPlayer
+    {
+        public Basketballer(string name) : base(name) { }
+
+        public void Shoot()
+        {
+            Console.WriteLine($"{Name} бросает мяч в корзину");
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Баскетболист: {Name}");
+        }
+    }
+
+    //Класс футболиста
+    public class Footballer : TeamMember, IFootballPlayer
+    {
+        public Footballer(string name) : base(name) { }
+
+        public void Kick()
+        {
+            Console.WriteLine($"{Name} бьёт по мячу");
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Футболист: {Name}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            TeamMember[] team =
+            {
+                new Basketballer("Иван"),
+                new Footballer("Алексей"),
+                new Basketballer("Олег"),
+                new Footballer("Руслан")
+            };
+
+            Console.WriteLine("Все игроки:");
+            foreach (TeamMember member in team)
+            {
+                member.ShowInfo();
+            }
+
+            Console.WriteLine("\nБаскетболисты:");
+
+            //Бизнес-логика: фильтрация по интерфейсу
+            foreach (TeamMember member in team)
+            {
+                if (member is IBasketballPlayer basketballPlayer)
+                {
+                    basketballPlayer.Shoot();
+                }
+            }
+        }
+    }
+}
---
+++ Lab 5/Zadanie 4/Zadanie 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LoggingAppZadanie
+{
+    //Интерфейс ошибок
+    public interface IErrorLogger
+    {
+        void Log(string message);
+    }
+
+    //Интерфейс событий
+    public interface IEventLogger
+    {
+        void Log(string message);
+    }
+
+    //Класс, реализующий оба интерфейса
+    public class Logger : IErrorLogger, IEventLogger
+    {
+        void IErrorLogger.Log(string message)
+        {
+            Console.WriteLine($"[ERROR]: {message}");
+        }
+        
+        void IEventLogger.Log(string message)
+        {
+            Console.WriteLine($"[EVENT]: {message}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Logger logger = new Logger();
+
+            //Доступ через интерфейс IErrorLogger
+            IErrorLogger errorLogger = logger;
+            errorLogger.Log("Ошибка подключения к базе данных");
+            
+            IEventLogger eventLogger = logger;
+            eventLogger.Log("Пользователь вошёл в систему");
+        }
+    }
+}
