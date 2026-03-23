adf30c8496edd0eb4fcbcdbde6d39ccf62576ab4
Asland001 <artyem.senkevich@gmail.com>
2026-03-24 01:26:34 +0300

    Add files via upload

---
+++ Lab 6/Zadanie 3/Zadanie 3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace BatteryAppZadanie 
+{
+    public delegate void BatteryLowHandler(int level);
+
+    //Класс-издатель
+    public class BatteryMonitor
+    {
+        public int BatteryLevel { get; set; }
+
+        //Событие на основе делегата
+        public event BatteryLowHandler BatteryLow;
+
+        public BatteryMonitor(int level)
+        {
+            BatteryLevel = level;
+        }
+
+        //Метод проверки заряда
+        public void CheckBattery()
+        {
+            Console.WriteLine($"Текущий заряд: {BatteryLevel}%");
+
+            if (BatteryLevel < 21)
+            {
+                OnBatteryLow();
+            }
+        }
+
+        //Метод вызова события
+        protected void OnBatteryLow()
+        {
+            Console.WriteLine("Низкий заряд батареи!");
+
+            BatteryLow?.Invoke(BatteryLevel);
+        }
+    }
+
+    //Подписчик 1
+    public class PowerSaver
+    {
+        public void Activate(int level)
+        {
+            Console.WriteLine("Режим энергосбережения включён");
+        }
+    }
+
+    //Подписчик 2
+    public class UserNotifier
+    {
+        public void Notify(int level)
+        {
+            Console.WriteLine($"Пользователь предупреждён: заряд {level}%");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            BatteryMonitor monitor = new BatteryMonitor(20);
+
+            PowerSaver saver = new PowerSaver();
+            UserNotifier notifier = new UserNotifier();
+            
+            monitor.BatteryLow += saver.Activate;
+            monitor.BatteryLow += notifier.Notify;
+            
+            monitor.CheckBattery();
+        }
+    }
+}
---
+++ Lab 6/Zadanie 4/Zadanie 4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace LoginAppZadanie
+{
+    public class UserLoginManager
+    {
+        //Событие на основе EventHandler
+        public event EventHandler UserLoggedIn;
+
+        public void Login(string username)
+        {
+            Console.WriteLine($"Пользователь {username} вошёл в систему");
+            
+            OnUserLoggedIn();
+        }
+
+        protected virtual void OnUserLoggedIn()
+        {
+            UserLoggedIn?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    //Подписчик 1
+    public class SecuritySystem
+    {
+        public void CheckAccess(object sender, EventArgs e)
+        {
+            Console.WriteLine("SecuritySystem: доступ проверен");
+        }
+    }
+
+    //Подписчик 2
+    public class NotificationService
+    {
+        public void SendNotification(object sender, EventArgs e)
+        {
+            Console.WriteLine("NotificationService: уведомление отправлено");
+        }
+    }
+
+    //Промежуточный класс (Observer)
+    public class LoginObserver
+    {
+        private readonly UserLoginManager _manager;
+        private readonly SecuritySystem _security;
+        private readonly NotificationService _notification;
+
+        public LoginObserver(UserLoginManager manager,
+                             SecuritySystem security,
+                             NotificationService notification)
+        {
+            _manager = manager;
+            _security = security;
+            _notification = notification;
+            
+            _manager.UserLoggedIn += _security.CheckAccess;
+            _manager.UserLoggedIn += _notification.SendNotification;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            UserLoginManager manager = new UserLoginManager();
+
+            SecuritySystem security = new SecuritySystem();
+            NotificationService notification = new NotificationService();
+            
+            LoginObserver observer = new LoginObserver(manager, security, notification);
+            
+            manager.Login("Island");
+        }
+    }
+}
