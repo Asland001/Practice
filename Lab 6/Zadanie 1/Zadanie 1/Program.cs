923536e4da304ca4a84aa719f757bba4a4327792
Asland001 <artyem.senkevich@gmail.com>
2026-03-24 01:25:50 +0300

    Add files via upload

---
+++ Lab 6/Zadanie 1/Zadanie 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace FileDelegateAppZadanie
+{
+    //Пользовательский делегат
+    public delegate void FileHandler(string path);
+
+    //Класс для чтения файла
+    public class FileReader
+    {
+        public void ReadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                string content = File.ReadAllText(path);
+                Console.WriteLine("Содержимое файла:");
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден");
+            }
+        }
+    }
+
+    //Класс для записи в файл
+    public class FileWriter
+    {
+        public void WriteFile(string path)
+        {
+            File.WriteAllText(path, "Fireflies\nТрек – Owl City 2009 г.");
+            Console.WriteLine("Данные записаны в файл");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            string path = "test.txt";
+
+            FileReader reader = new FileReader();
+            FileWriter writer = new FileWriter();
+            
+            FileHandler handler;
+
+            handler = writer.WriteFile;
+            handler(path);
+
+            Console.WriteLine();
+            
+            handler = reader.ReadFile;
+            handler(path);
+        }
+    }
+}
---
+++ Lab 6/Zadanie 2/Zadanie 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ImageProcessingAppZadanie
+{
+    public delegate void ImageProcessor(string path);
+
+    public class ImageService
+    {
+        //Метод, принимающий делегат (callback)
+        public void ProcessImage(string path, ImageProcessor processor)
+        {
+            Console.WriteLine($"Обработка изображения: {path}");
+            processor(path);
+            Console.WriteLine("Обработка завершена\n");
+        }
+
+        //Метод 1: изменение размера
+        public void ResizeImage(string path)
+        {
+            Console.WriteLine($"Изображение {path} изменено по размеру");
+        }
+
+        //Метод 2: перевод в ч/б
+        public void ConvertToGrayscale(string path)
+        {
+            Console.WriteLine($"Изображение {path} преобразовано в чёрно-белое");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ImageService service = new ImageService();
+
+            string imagePath = "D:\\Флешка\\Practice\\Lab 6\\Zadanie 2\\Zadanie 2\\bin\\Debug\\net8.0\\photo.jpg";
+            
+            service.ProcessImage(imagePath, service.ResizeImage);
+            
+            service.ProcessImage(imagePath, service.ConvertToGrayscale);
+        }
+    }
+}
