6237af7e8b577e91c017e109e2b55db252cd05e3
Asland001 <artyem.senkevich@gmail.com>
2026-03-25 22:06:46 +0300

    Add files via upload

---
+++ Lab 8/Zadanie 1/Zadanie 1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+
+namespace HotelAppZadanie
+{
+    //Модельный класс
+    public class HotelRoom
+    {
+        public int RoomNumber { get; set; }
+        public string GuestName { get; set; }
+
+        public HotelRoom(int roomNumber, string guestName)
+        {
+            RoomNumber = roomNumber;
+            GuestName = guestName;
+        }
+
+        public override string ToString()
+        {
+            return $"Номер: {RoomNumber}, Гость: {GuestName}";
+        }
+    }
+
+    //Класс-менеджер
+    public class HotelReservationSystem
+    {
+        private Hashtable _rooms = new Hashtable();
+        
+        public void AddReservation(int roomNumber, string guestName)
+        {
+            if (_rooms.ContainsKey(roomNumber))
+            {
+                Console.WriteLine("Номер уже занят");
+                return;
+            }
+
+            _rooms.Add(roomNumber, new HotelRoom(roomNumber, guestName));
+            Console.WriteLine("Бронирование добавлено");
+        }
+        
+        public void RemoveReservation(int roomNumber)
+        {
+            if (_rooms.ContainsKey(roomNumber))
+            {
+                _rooms.Remove(roomNumber);
+                Console.WriteLine("Бронирование удалено");
+            }
+            else
+            {
+                Console.WriteLine("Номер не найден");
+            }
+        }
+        
+        public void FindReservation(int roomNumber)
+        {
+            if (_rooms.ContainsKey(roomNumber))
+            {
+                Console.WriteLine(_rooms[roomNumber]);
+            }
+            else
+            {
+                Console.WriteLine("Номер не найден");
+            }
+        }
+        
+        public void ShowAll()
+        {
+            Console.WriteLine("Список бронирований:");
+
+            foreach (DictionaryEntry entry in _rooms)
+            {
+                Console.WriteLine(entry.Value);
+            }
+        }
+
+        //Фильтрация по имени
+        public void FilterByGuest(string guestName)
+        {
+            Console.WriteLine($"Поиск по имени: {guestName}");
+
+            foreach (DictionaryEntry entry in _rooms)
+            {
+                HotelRoom room = (HotelRoom)entry.Value;
+
+                if (room.GuestName.Equals(guestName, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(room);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            HotelReservationSystem system = new HotelReservationSystem();
+            
+            system.AddReservation(101, "Руслан");
+            system.AddReservation(102, "Алан");
+            system.AddReservation(103, "Эндрю");
+            
+            system.ShowAll();
+
+            Console.WriteLine();
+            
+            system.FilterByGuest("Эндрю");
+
+            Console.WriteLine();
+            
+            system.RemoveReservation(101);
+
+            Console.WriteLine();
+
+            system.ShowAll();
+        }
+    }
+}
---
+++ Lab 8/Zadanie 2/Zadanie 2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyBagAppZadanie
+{
+    //Дженерик-коллекция
+    public class MyBag<T>
+    {
+        private List<T> _items = new List<T>();
+        
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+        
+        public bool Remove(T item)
+        {
+            return _items.Remove(item);
+        }
+
+        //Поиск
+        public bool Contains(T item)
+        {
+            return _items.Contains(item);
+        }
+
+        //Количество элементов
+        public int Count()
+        {
+            return _items.Count;
+        }
+        
+        public void ShowAll()
+        {
+            Console.WriteLine("Содержимое сумки:");
+            foreach (T item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    //Класс-контроллер
+    public class BagManager<T>
+    {
+        private MyBag<T> _bag = new MyBag<T>();
+
+        public void AddItem(T item)
+        {
+            _bag.Add(item);
+            Console.WriteLine($"Добавлен предмет: {item}");
+        }
+
+        public void RemoveItem(T item)
+        {
+            if (_bag.Remove(item))
+            {
+                Console.WriteLine($"Удалён предмет: {item}");
+            }
+            else
+            {
+                Console.WriteLine($"Предмет не найден: {item}");
+            }
+        }
+
+        public void CheckItem(T item)
+        {
+            if (_bag.Contains(item))
+            {
+                Console.WriteLine($"Предмет есть в сумке: {item}");
+            }
+            else
+            {
+                Console.WriteLine($"Предмет отсутствует: {item}");
+            }
+        }
+
+        public void ShowInventory()
+        {
+            _bag.ShowAll();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            BagManager<string> manager = new BagManager<string>();
+            
+            manager.AddItem("Меч");
+            manager.AddItem("Зелье");
+            manager.AddItem("Меч");
+
+            Console.WriteLine();
+            
+            manager.CheckItem("Меч");
+
+            Console.WriteLine();
+
+            manager.ShowInventory();
+
+            Console.WriteLine();
+            
+            manager.RemoveItem("Меч");
+
+            Console.WriteLine();
+
+            manager.ShowInventory();
+        }
+    }
+}
