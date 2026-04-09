934b4a1249ed2ddd8f02301804803c12d8fd9a90
Asland001 <artyem.senkevich@gmail.com>
2026-04-09 11:37:40 +0300

    Add files via upload

---
+++ Lab 16/CinemaApp/CinemaApp/Models/SeatModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CinemaApp.Models
+{
+    public class SeatModel : INotifyPropertyChanged
+    {
+        public int Number { get; set; }
+
+        private bool isBooked;
+        public bool IsBooked
+        {
+            get => isBooked;
+            set
+            {
+                if (isBooked == value) return;
+                isBooked = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(StatusText));
+            }
+        }
+
+        public string StatusText => IsBooked ? "Занято" : "Свободно";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
